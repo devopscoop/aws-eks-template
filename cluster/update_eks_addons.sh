05780931dae8fc768e9dc1ab0978acf24f10d312
nxatdo <nxatdo@noreply.codeberg.org>
2025-10-09 08:32:09 -0700

    Making update script smarter

--- cluster/update_eks_addons.sh
+++ cluster/update_eks_addons.sh
@@ -11,10 +11,12 @@ AWS_REGION=$(sed -nE "s/^region[^=]*=[ \t]+['\"]?([^'\"]+)['\"]?/\1/p" "${SCRIPT
 export AWS_REGION
 cluster_version=$(sed -nE "s/^cluster_version[^=]*=[ \t]+['\"]?([^'\"]+)['\"]?/\1/p" "${SCRIPT_DIR}/terraform.tfvars")
 export cluster_version
-for eks_addon in aws-ebs-csi-driver coredns eks-pod-identity-agent kube-proxy vpc-cni; do
+
+for eks_addon in $(sed -nE 's/^eks_addon_version_([A-Za-z0-9-]+).*/\1/p' "${SCRIPT_DIR}"/terraform.tfvars); do
   # echo "eks_addon_version_${eks_addon} = \"$(aws eks describe-addon-versions --addon-name "$eks_addon" --kubernetes-version "$cluster_version" --query "addons[].addonVersions[].addonVersion" | jq -r '.[0]')\""
   eks_addon_version=$(aws eks describe-addon-versions --addon-name "$eks_addon" --kubernetes-version "$cluster_version" --query "addons[].addonVersions[].addonVersion" | jq -r '.[0]')
   sed -i.bak -E "s/^eks_addon_version_${eks_addon}[^=]*=[ \t]+['\"]?([^'\"]+)['\"]?/eks_addon_version_${eks_addon} = \"${eks_addon_version}\"/" "${SCRIPT_DIR}/terraform.tfvars"
   rm "${SCRIPT_DIR}/terraform.tfvars.bak"
 done
+
 tofu fmt "${SCRIPT_DIR}/terraform.tfvars"
